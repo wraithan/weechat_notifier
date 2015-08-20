0aa7568ab15c313a991c94652612eae027dfb93d
Wraithan (Chris McDonald) <wmcdonald@newrelic.com>
2015-08-19 18:23:10 -0700

    use if/let for single match

--- weechat_parser/tests/sample-session.rs
+++ weechat_parser/tests/sample-session.rs
@@ -24,18 +24,16 @@ fn simple_session_test () {
 
     let message = WeechatMessage::from_raw_message(&buffer[..length]).unwrap();
     assert_eq!(message.id, "_buffer_line_added");
-    let datum = message.data.get(0).unwrap();
-    match datum {
-        &WeechatData::Hdata(ref name, ref data) => {
-            assert_eq!(name, "line_data");
-            assert_eq!(data.len(), 1);
-            let hdata = data.get(0).unwrap();
-            assert_eq!(hdata.get("buffer").unwrap(), &WeechatData::Pointer("0x7fcab15936d0".to_owned()));
-            assert_eq!(hdata.get("date").unwrap(), &WeechatData::Time("1439651878".to_owned()));
-            assert_eq!(hdata.get("date_printed").unwrap(), &WeechatData::Time("1439651878".to_owned()));
-            assert_eq!(hdata.get("displayed").unwrap(), &WeechatData::Char('\u{1}'));
-            assert_eq!(hdata.get("highlight").unwrap(), &WeechatData::Char('\u{0}'));
-        },
-        _ => panic!("unexpected type: {:?}", datum)
+    if let &WeechatData::Hdata(ref name, ref data) = message.data.get(0).unwrap() {
+        assert_eq!(name, "line_data");
+        assert_eq!(data.len(), 1);
+        let hdata = data.get(0).unwrap();
+        assert_eq!(hdata.get("buffer").unwrap(), &WeechatData::Pointer("0x7fcab15936d0".to_owned()));
+        assert_eq!(hdata.get("date").unwrap(), &WeechatData::Time("1439651878".to_owned()));
+        assert_eq!(hdata.get("date_printed").unwrap(), &WeechatData::Time("1439651878".to_owned()));
+        assert_eq!(hdata.get("displayed").unwrap(), &WeechatData::Char('\u{1}'));
+        assert_eq!(hdata.get("highlight").unwrap(), &WeechatData::Char('\u{0}'));
+    } else {
+        panic!("unexpected type for first message");
     }
 }
